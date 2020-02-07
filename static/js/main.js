14d7ab16c8a598e4ff0d15258aaf05ccc6e16852
Joe Davis <davisj38@michigan.gov>
2020-02-07 14:49:38 -0500

    Upload initial content

---
+++ static/js/main.js
@@ -0,0 +1,46 @@
+
+
+// OnLoad Run
+window.addEventListener('load', function() {
+    InitNavigationMenu();
+});
+
+
+function InitNavigationMenu() {
+    let nav_toggler = document.querySelector('.navbar-toggler');
+    let nav_popup = document.querySelector('.navbar-popup');
+
+    nav_toggler.addEventListener('click', function() {
+        if (nav_popup.classList.contains('navbar-popup-show')) {
+            nav_toggler.classList.remove('navbar-toggler-expand');
+            nav_popup.classList.remove('navbar-popup-show');
+        }
+        else {
+            nav_toggler.classList.add('navbar-toggler-expand');
+            nav_popup.classList.add('navbar-popup-show');
+        }
+    });
+
+    // close menu popup on mousedown outside of menu popup
+    document.addEventListener('mousedown', function(event) {
+        if (nav_popup.classList.contains('navbar-popup-show')) {
+            contains_login_popup = false;
+            node = event.target;
+
+            // check event.target parents for menu popup and menu toggler
+            while (node !== null) {
+                if (node === nav_popup || node === nav_toggler) {
+                    contains_login_popup = true;
+                }
+                node = node.parentElement;
+            }
+
+            // if outside of menu popup, close menu popup and flip chevron
+            if (!contains_login_popup) {
+                nav_toggler.classList.remove('navbar-toggler-expand');
+                nav_popup.classList.remove('navbar-popup-show');
+            }
+        }
+    });
+}
+
