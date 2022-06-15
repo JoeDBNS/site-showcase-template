7d74ad3753466d845ff00c8c0896e702b63267f6
Joe Davis <Joedbns@gmail.com>
2022-06-14 22:24:47 -0400

    Update npm, gulp, and resolve issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
@@ -10,7 +10,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/scss/**/*.scss', ['sass']);
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('build', gulp.series('sass'));
